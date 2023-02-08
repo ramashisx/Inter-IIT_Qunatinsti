efb26dd982e2bddabcb1124026cecf12a29b21b4
yoraghav <82400899+yoraghav@users.noreply.github.com>
2023-02-08 21:47:05 +0530

    more data

--- scraper.py
+++ scraper.py
@@ -1,7 +1,21 @@
-from pandas_datareader import data as pdr
-
+# from pandas_datareader import data as pdr
+import pandas as pd
 import yfinance as yf
 
-data = yf.download("^NSEBANK", start="2001-07-01", end="2021-07-30")
+tickers = pd.read_csv("190.csv")
+# tickers = tickers.Tickers
 
-data.to_csv("BANKNIFTYfull.csv")
+a=0
+print(tickers.loc[1])
+for i in range(len(tickers)):
+    x = str(tickers.loc[i].Company)
+    try:
+        data = yf.download(x+".NS", start="2014-07-01", end="2017-07-01",interval = '1d')
+        data.to_csv("rendidata/period1/"+x+".csv")
+        data = yf.download(x+".NS", start="2017-07-01", end="2021-07-01",interval = '1d')
+        data.to_csv("rendidata/period2/"+x+".csv")
+        data = yf.download(x+".NS", start="2021-07-01", end="2023-02-01",interval = '1d')
+        data.to_csv("rendidata/period3/"+x+".csv")
+        # print('aa')
+    except:
+        print(x)
